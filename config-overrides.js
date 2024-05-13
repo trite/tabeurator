b88d5de308f69171c06bd5ae74eb519ab4df29ad
trite <trite@users.noreply.github.com>
2024-05-13 07:00:35 -0700

    wording

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 module.exports = function override(config, env) {
-  // Do not use the production build in watch mode
+  // Don't minify in development
   if (env === 'development') {
     config.optimization.minimize = false;
   }
